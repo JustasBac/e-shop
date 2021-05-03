5d71df78c0fe7d55e54d1cbdcd01bf4c2e3897f4
JustasBac <justas.bac@outlook.com>
2021-05-03 19:23:16 +0200

    commerce js connected

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Products from './components/products/products'
+import Products from './components/Products/Products'
+import { commerce } from './lib/commerce'
+
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []) //runs only at the start
+
     return (
         <div>
             <Navbar />
-            <Products />
+            <Products products={products} />
         </div>
     )
 }
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { Grid } from '@material-ui/core'
 
 import Product from './Product/Product'
+import useStyles from './styles'
 
-const products = [
-    { id: 1, name: 'Phone', description: 'Samsung A10', price: '350€', image: 'https://www.allround-pc.com/wp-content/uploads/2019/03/Samsung-Galaxy-S10-und-S10-11-1112x625.jpg' },
-    { id: 2, name: 'Computer', description: 'MacBook Air', price: '2100€', image: 'https://cdn.pocket-lint.com/r/s/970x/assets/images/152137-laptops-review-apple-macbook-pro-2020-review-image1-pbzm4ejvvs.jpg' }
-]
 
-const Products = () => {
-    console.log(products)
+const Products = ({ products }) => {
+    const classes = useStyles();
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
